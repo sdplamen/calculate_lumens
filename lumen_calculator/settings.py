48ffb494bc9aa80ced37ac2722ca6816ce60c544
Plamen Stoilkov <sdplamen@gmail.com>
2025-10-14 16:06:36 +0300

    Update dependenties

--- lumen_calculator/settings.py
+++ lumen_calculator/settings.py
@@ -135,6 +135,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 SWAGGER_SETTINGS = {
     'USE_SESSION_AUTH': False,
